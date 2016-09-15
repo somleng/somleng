b1cf2b568e8dea75066223e4c24b34390f599e43
Leanghak Vang <alexphnompenh@gmail.com>
2016-09-15 13:02:59 +0700

    Setup Swap for Elastic Beanstalk

---
+++ .ebextensions/01_setup_swap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#http://steinn.org/post/elasticbeanstalk-swap/
+
+SWAPFILE=/var/swapfile
+SWAP_MEGABYTES=2048
+
+if [ -f $SWAPFILE ]; then
+  echo "Swapfile $SWAPFILE found, assuming already setup"
+  exit;
+fi
+
+/bin/dd if=/dev/zero of=$SWAPFILE bs=1M count=$SWAP_MEGABYTES
+/bin/chmod 600 $SWAPFILE
+/sbin/mkswap $SWAPFILE
+/sbin/swapon $SWAPFILE
