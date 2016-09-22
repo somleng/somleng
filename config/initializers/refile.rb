58d1e04fdd0b98ffc30bff63768c5bffe32bb9ff
Leanghak Vang <alexphnompenh@gmail.com>
2016-09-22 13:14:49 +0700

    Change ENV var names for s3 configuration

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -2,13 +2,12 @@ if ENV["CDR_STORAGE"] == "s3" && Rails.env.production?
   require "refile/s3"
 
   aws = {
-    :access_key_id => ENV["AWS_S3_ACCESS_KEY_ID"],
-    :secret_access_key => ENV["AWS_S3_SECRET_ACCESS_KEY"],
-    :region => ENV["AWS_S3_REGION"],
-    :bucket => ENV["AWS_S3_BUCKET"],
-    :max_size => (ENV["AWS_S3_MAX_FILE_SIZE_MB"] || 1).to_i.megabytes
+    :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+    :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
+    :region => ENV["AWS_REGION"],
+    :bucket => ENV["AWS_S3_REFILE_BUCKET"],
+    :max_size => (ENV["AWS_S3_REFILE_MAX_FILE_SIZE_MB"] || 1).to_i.megabytes
   }
 
-  Refile.cache = nil
-  Refile.store = Refile::S3.new(:prefix => ENV["AWS_S3_STORE_PREFIX"], **aws)
+  Refile.store = Refile::S3.new(:prefix => ENV["AWS_S3_REFILE_STORE_PREFIX"], **aws)
 end
