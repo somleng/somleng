6746a7087bf0b155900275cede56187763b0427b
David Wilkie <dwilkie@gmail.com>
2017-10-20 16:41:15 +0700

    Update seeds file to allow creating admin file with all possible permissions. #33

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-account = Account.first_or_create!
+account = Account.without_permissions.first_or_create!
 access_token = account.access_token || account.create_access_token!
 
 puts "User Account SID:         #{account.sid}"
@@ -7,10 +7,19 @@ puts "User Account Auth Token:  #{account.auth_token}"
 if ENV["CREATE_ADMIN_ACCOUNT"].to_i == 1
   if !(raw_account_permissions = ENV["ADMIN_ACCOUNT_PERMISSIONS"])
     puts "Specify ADMIN_ACCOUNT_PERMISSIONS=comma_separated_list_of_permissions"
-    puts "Possible Permissions are: #{Account.values_for_permissions.join(',')}"
+    puts "Possible Permissions are: #{Account.values_for_permissions.to_sentence}"
   else
     account_permissions = raw_account_permissions.split(",").map(&:to_sym)
-    admin_account = Account.with_permissions(*account_permissions).first_or_initialize
+    account_permissions = Account.values_for_permissions if account_permissions == [:all]
+
+    account_permissions = account_permissions.zip(
+      account_permissions
+    ).to_h.slice(
+      *Account.values_for_permissions
+    ).values
+
+    admin_account = (account_permissions.any? ? Account.with_permissions(*account_permissions) : Account.without_permissions).first_or_initialize
+
     if admin_account.new_record?
       admin_account.permissions = account_permissions
       admin_account.save!
--- docs/GETTING_STARTED.md
+++ docs/GETTING_STARTED.md
@@ -12,8 +12,8 @@ $ sudo docker-compose run web  /bin/bash -c './bin/rails db:create && ./bin/rail
 
 ### Seed the database
 
-Run the following command and note down the output.
+Run the following command and note down the output. This will create a user account and an admin account with all possible permissions. For a production environment it's recommended that you create individual Admin users for each specific task.
 
 ```
-$ sudo docker-compose run web /bin/bash -c './bin/rails db:seed'
+$ sudo docker-compose run -e CREATE_ADMIN_ACCOUNT=1 -e ADMIN_ACCOUNT_PERMISSIONS=all web /bin/bash -c './bin/rails db:seed'
 ```
