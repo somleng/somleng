9bd82d7c192e92161a9e4baa540ceabee6d3e2e6
David Wilkie <dwilkie@gmail.com>
2017-12-06 11:29:24 +0700

    Don't specify a max_size for a file if config not specified. Closes #39

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -5,9 +5,12 @@ if ENV["CDR_STORAGE"] == "s3" && Rails.env.production?
     :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
     :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
     :region => ENV["AWS_REGION"],
-    :bucket => ENV["AWS_S3_REFILE_BUCKET"],
-    :max_size => (ENV["AWS_S3_REFILE_MAX_FILE_SIZE_MB"] || 1).to_i.megabytes
+    :bucket => ENV["AWS_S3_REFILE_BUCKET"]
   }
+
+  if max_size = ENV["AWS_S3_REFILE_MAX_FILE_SIZE_MB"]
+    aws.merge!(:max_size => max_size.to_i.megabytes)
+  end
 
   Refile.store = Refile::S3.new(:prefix => ENV["AWS_S3_REFILE_STORE_PREFIX"], **aws)
 end
