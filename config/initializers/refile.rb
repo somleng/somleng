1d30b3693fec16a6d48a1144b20ab39a6fe98811
David Wilkie <dwilkie@gmail.com>
2018-04-17 17:50:52 +0700

    Fix incorrect env variable in refile config

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
   require 'refile/s3'
 
   aws = {
-    access_key_id: Rails.application.secrets.fetch(:aws_access_key_id),
-    secret_access_key: Rails.application.secrets.fetch(:aws_secret_access_key),
+    access_key_id: Rails.application.secrets.fetch(:aws_s3_access_key_id),
+    secret_access_key: Rails.application.secrets.fetch(:aws_s3_secret_access_key),
     region: Rails.application.secrets.fetch(:aws_region),
     bucket: Rails.application.secrets.fetch(:uploads_bucket)
   }
