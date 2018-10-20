d989b2c18a4b06ef61020f6138778404b573104a
David Wilkie <dwilkie@gmail.com>
2018-10-20 10:20:47 +0700

    Refactor SNS spec

--- app/controllers/api/admin/aws_sns_messages_controller.rb
+++ app/controllers/api/admin/aws_sns_messages_controller.rb
@@ -1,10 +1,10 @@
 class Api::Admin::AwsSnsMessagesController < Api::Admin::BaseController
-  PERMITTED_HEADERS = [
-    "HTTP_X_AMZ_SNS_MESSAGE_TYPE",
-    "HTTP_X_AMZ_SNS_MESSAGE_ID",
-    "HTTP_X_AMZ_SNS_TOPIC_ARN",
-    "HTTP_X_AMZ_SNS_SUBSCRIPTION_ARN"
-  ]
+  PERMITTED_HEADERS = %w[
+    HTTP_X_AMZ_SNS_MESSAGE_TYPE
+    HTTP_X_AMZ_SNS_MESSAGE_ID
+    HTTP_X_AMZ_SNS_TOPIC_ARN
+    HTTP_X_AMZ_SNS_SUBSCRIPTION_ARN
+  ].freeze
 
   def create
     AwsSnsMessageProcessorJob.perform_later(permitted_headers, request.raw_post)
@@ -12,10 +12,6 @@ class Api::Admin::AwsSnsMessagesController < Api::Admin::BaseController
   end
 
   private
-
-  def permission_name
-    :manage_aws_sns_messages
-  end
 
   def permitted_headers
     request.headers.to_h.slice(*PERMITTED_HEADERS)
