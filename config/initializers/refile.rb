5ce25efdeca3845617a929049f569b947e977fa3
David Wilkie <dwilkie@gmail.com>
2018-11-13 05:17:32 +0700

    Setup Sentry (#181)
    * Setup sentry

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
   require "refile/s3"
 
   aws = {
-    access_key_id: AppConfig.fetch(:aws_s3_access_key_id),
-    secret_access_key: AppConfig.fetch(:aws_s3_secret_access_key),
-    region: AppConfig.fetch(:aws_region),
-    bucket: AppConfig.fetch(:uploads_bucket)
+    access_key_id: Rails.configuration.app_settings.fetch("aws_s3_access_key_id"),
+    secret_access_key: Rails.configuration.app_settings.fetch("aws_s3_secret_access_key"),
+    region: Rails.configuration.app_settings.fetch("aws_region"),
+    bucket: Rails.configuration.app_settings.fetch("uploads_bucket")
   }
 
   Refile.store = Refile::S3.new(prefix: "store", **aws)
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,8 @@
+if (sentry_dsn = Rails.configuration.app_settings["sentry_dsn"].presence)
+  Raven.configure do |config|
+    config.dsn = sentry_dsn
+    config.environments = %w[production]
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    config.async = ->(event) { SentryJob.perform_later(event.to_hash) }
+  end
+end
