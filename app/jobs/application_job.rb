6412693180de488527c88b2525a775a4aaeeefec
David Wilkie <dwilkie@gmail.com>
2018-11-13 06:21:16 +0700

    Queue by name instead of URL

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,5 +3,5 @@ class ApplicationJob < ActiveJob::Base
     Rails.configuration.app_settings["#{to_s.underscore}_queue_url"] || Rails.configuration.app_settings.fetch("default_queue_url")
   end
 
-  queue_as(aws_sqs_queue_url)
+  queue_as(aws_sqs_queue_url.split("/").last)
 end
