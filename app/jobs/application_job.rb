c9c365b03c4f485e0844a439100364848188367a
David Wilkie <dwilkie@gmail.com>
2018-11-13 05:38:39 +0700

    Fix implementation of queue name

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,8 +1,6 @@
 class ApplicationJob < ActiveJob::Base
   def self.aws_sqs_queue_url
-    Rails.configuration.app_settings.fetch("#{to_s.underscore}_queue_url") do
-      Rails.configuration.app_settings.fetch("default_queue_url")
-    end
+    Rails.configuration.app_settings["#{to_s.underscore}_queue_url"] || Rails.configuration.app_settings.fetch("default_queue_url")
   end
 
   queue_as(aws_sqs_queue_url)
