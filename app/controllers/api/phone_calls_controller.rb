699fdf5b2be5bd9a200c66f3ba73e26d8c3cdb68
David Wilkie <dwilkie@gmail.com>
2019-07-20 09:45:13 +0700

    Remove namespace

--- app/controllers/api/phone_calls_controller.rb
+++ app/controllers/api/phone_calls_controller.rb
@@ -3,7 +3,7 @@ module API
     def create
       schema_validation_result = PhoneCallRequestSchema.call(request.params)
       if schema_validation_result.success?
-        phone_call = API::CreatePhoneCall.call(current_account, schema_validation_result.output)
+        phone_call = CreatePhoneCall.call(current_account, schema_validation_result.output)
         respond_with(phone_call, location: api_twilio_account_call_url(current_account, phone_call))
       else
         respond_with(schema_validation_result)
