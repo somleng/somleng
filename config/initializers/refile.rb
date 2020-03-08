1faddc0244dc6d5aa2b69f0566718970898ce8df
David Wilkie <dwilkie@gmail.com>
2020-03-08 13:55:09 +0700

    Remove AWS Access Keys for buckets

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -2,8 +2,6 @@ if Rails.env.production?
   require "refile/s3"
 
   aws = {
-    access_key_id: Rails.configuration.app_settings.fetch(:aws_s3_access_key_id),
-    secret_access_key: Rails.configuration.app_settings.fetch(:aws_s3_secret_access_key),
     region: Rails.configuration.app_settings.fetch(:aws_region),
     bucket: Rails.configuration.app_settings.fetch(:uploads_bucket)
   }
