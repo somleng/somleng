17c1a0632da5de47cbd9a43d4d851ad76c5d5eca
David Wilkie <dwilkie@gmail.com>
2020-03-25 19:32:02 +0700

    Setup cache to point to S3

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,9 +1,11 @@
 if Rails.env.production?
   require "refile/s3"
 
-  Refile.store = Refile::S3.new(
-    prefix: "store",
+  aws = {
     region: Rails.configuration.app_settings.fetch(:aws_region),
     bucket: Rails.configuration.app_settings.fetch(:uploads_bucket)
-  )
+  }
+
+  Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+  Refile.store = Refile::S3.new(prefix: "store", **aws)
 end
