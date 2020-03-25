23b6751db36fca1d7cfb75a8da74c904ff826da2
David Wilkie <dwilkie@gmail.com>
2020-03-25 14:29:26 +0700

    Use default queue

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -8,4 +8,6 @@ class ApplicationJob < ActiveJob::Base
   def self.aws_sqs_queue_url
     Rails.configuration.app_settings[:"#{to_s.underscore}_queue_url"] || Rails.configuration.app_settings.fetch(:default_queue_url)
   end
+
+  queue_as(aws_sqs_queue_url.split("/").last)
 end
