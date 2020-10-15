79d29478767e8a9e08c5e662085aa667f517e311
David Wilkie <dwilkie@gmail.com>
2020-10-15 16:08:43 +0700

    Don't remove refile columns

--- db/migrate/20200905055706_remove_price_from_call_data_records.rb
+++ db/migrate/20200905055706_remove_price_from_call_data_records.rb
@@ -2,9 +2,5 @@ class RemovePriceFromCallDataRecords < ActiveRecord::Migration[6.0]
   def change
     remove_column(:call_data_records, :price_microunits)
     remove_column(:call_data_records, :price_currency)
-    remove_column(:call_data_records, :file_id)
-    remove_column(:call_data_records, :file_filename)
-    remove_column(:call_data_records, :file_size)
-    remove_column(:call_data_records, :file_content_type)
   end
 end
