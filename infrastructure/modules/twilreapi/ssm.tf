dfdf320871fbbcd9da66f20705d859520b2f2e61
Samnang Chhun <samnang.chhun@gmail.com>
2021-01-23 17:03:25 +0700

    Add services password to infrastructure

--- infrastructure/modules/twilreapi/ssm.tf
+++ infrastructure/modules/twilreapi/ssm.tf
@@ -1,12 +1,17 @@
-resource "random_password" "rails_master_key" {
-  length = 32
-  special = true
-}
-
 resource "aws_ssm_parameter" "rails_master_key" {
   name  = "twilreapi.${var.app_environment}.rails_master_key"
   type  = "SecureString"
-  value = random_password.rails_master_key.result
+  value = "change-me"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "services_password" {
+  name  = "twilreapi.${var.app_environment}.services_password"
+  type  = "SecureString"
+  value = "change-me"
 
   lifecycle {
     ignore_changes = [value]
