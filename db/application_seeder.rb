f49b78d226ac2e9a08e8de4c8d752af60b6af3fd
Samnang Chhun <samnang.chhun@gmail.com>
2021-03-22 11:00:08 +0700

    Update seeder

--- db/application_seeder.rb
+++ db/application_seeder.rb
@@ -1,10 +1,18 @@
 class ApplicationSeeder
   def seed!
-    account = Account.first_or_create!
-    account.access_token || account.create_access_token!
+    account = Account.first_or_create! do |record|
+      record.build_access_token
+      record.incoming_phone_numbers.build(
+        phone_number: "1234",
+        voice_url: "https://demo.twilio.com/docs/voice.xml",
+        voice_method: "GET"
+      )
+    end
 
-    print(
-      "Account SID:         #{account.id}\nAuth Token:          #{account.auth_token}"
-    )
+    puts(<<~INFO)
+      Account SID:          #{account.id}
+      Auth Token:           #{account.auth_token}
+      Inbound Phone Number: #{account.incoming_phone_numbers.first.phone_number}
+    INFO
   end
 end
