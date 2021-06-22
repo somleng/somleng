65f752c9d08f3f14b6322208cb97a72ce6126385
David Wilkie <dwilkie@gmail.com>
2021-06-22 16:03:09 +0700

    Fix sentry initializer

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,4 @@
-if (sentry_dsn = Rails.configuration.app_settings[:sentry_dsn].presence)
-  Raven.configure do |config|
-    config.dsn = sentry_dsn
-    config.environments = %w[production]
-    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-    config.async = ->(event, hint) { Sentry::SendEventJob.perform_later(event, hint) }
-  end
+Sentry.init do |config|
+  config.dsn = Rails.configuration.app_settings[:sentry_dsn]
+  config.async = ->(event, hint) { Sentry::SendEventJob.perform_later(event, hint) }
 end
