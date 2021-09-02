5899b35917dd131520742467c20254787b6e2b6f
David Wilkie <dwilkie@gmail.com>
2021-09-02 20:59:38 +0700

    Fix devise routes (#887)

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,66 @@
 Rails.application.routes.draw do
   devise_for :users, skip: %i[registrations invitations]
 
-  devise_scope :user do
-    resource(
-      :registration,
-      only: %i[edit update],
-      controller: "users/registrations",
-      as: :user_registration,
-      path: "users"
-    )
+  constraints subdomain: "dashboard" do
+    devise_scope :user do
+      resource(
+        :registration,
+        only: %i[edit update],
+        controller: "users/registrations",
+        as: :user_registration,
+        path: "users"
+      )
 
-    resource(
-      :invitation,
-      only: :update,
-      controller: "users/invitations",
-      as: :user_invitation,
-      path: "users/invitation"
-    ) do
-      get :accept, action: :edit
+      resource(
+        :invitation,
+        only: :update,
+        controller: "users/invitations",
+        as: :user_invitation,
+        path: "users/invitation"
+      ) do
+        get :accept, action: :edit
+      end
+
+      root to: "dashboard/home#show"
     end
 
-    root to: "dashboard/home#show"
+    scope "/", module: :dashboard, as: :dashboard do
+      resources :two_factor_authentications, only: %i[new create destroy]
+      resources :accounts
+      resources :account_memberships
+      resources :outbound_sip_trunks
+      resources :users
+      resources :exports, only: %i[index create]
+      resource :account_session, only: :create
+      resource :account_settings, only: %i[show edit update]
+      resource :carrier_settings, only: %i[show edit update]
+      resource :home, only: :show
+      resources :user_invitations, only: :update
+      resources :phone_numbers
+      resources :phone_number_configuration, only: %i[edit update]
+      resources :inbound_sip_trunks
+      resources :phone_calls, only: %i[index show]
+
+      root to: "home#show"
+    end
+
+    namespace :admin do
+      concern :exportable do
+        get :export, on: :collection
+      end
+
+      resources :carriers, only: %i[show index], concerns: :exportable
+      resources :accounts, only: %i[show index], concerns: :exportable
+      resources :phone_calls, only: %i[show index], concerns: :exportable
+
+      resources :inbound_sip_trunks, only: :show
+      resources :outbound_sip_trunks, only: :show
+      resources :phone_numbers, only: :show
+      resources :phone_call_events, only: :show
+      resources :call_data_records, only: :show
+
+      root to: "phone_calls#index"
+    end
   end
 
   constraints subdomain: %w[api twilreapi] do
@@ -42,43 +82,5 @@ Rails.application.routes.draw do
         resources :accounts, only: %i[create show update index]
       end
     end
-  end
-
-  scope "/", module: :dashboard, as: :dashboard, constraints: { subdomain: "dashboard" } do
-    resources :two_factor_authentications, only: %i[new create destroy]
-    resources :accounts
-    resources :account_memberships
-    resources :outbound_sip_trunks
-    resources :users
-    resources :exports, only: %i[index create]
-    resource :account_session, only: :create
-    resource :account_settings, only: %i[show edit update]
-    resource :carrier_settings, only: %i[show edit update]
-    resource :home, only: :show
-    resources :user_invitations, only: :update
-    resources :phone_numbers
-    resources :phone_number_configuration, only: %i[edit update]
-    resources :inbound_sip_trunks
-    resources :phone_calls, only: %i[index show]
-
-    root to: "home#show"
-  end
-
-  namespace :admin do
-    concern :exportable do
-      get :export, on: :collection
-    end
-
-    resources :carriers, only: %i[show index], concerns: :exportable
-    resources :accounts, only: %i[show index], concerns: :exportable
-    resources :phone_calls, only: %i[show index], concerns: :exportable
-
-    resources :inbound_sip_trunks, only: :show
-    resources :outbound_sip_trunks, only: :show
-    resources :phone_numbers, only: :show
-    resources :phone_call_events, only: :show
-    resources :call_data_records, only: :show
-
-    root to: "phone_calls#index"
   end
 end
