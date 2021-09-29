1241937a984f3db339eb0d34d4dff042e40135f0
Samnang Chhun <samnang.chhun@gmail.com>
2021-09-29 15:40:30 +0700

    Fix incomplete exports (#898)

--- infrastructure/modules/twilreapi/sqs.tf
+++ infrastructure/modules/twilreapi/sqs.tf
@@ -1,20 +1,24 @@
 resource "aws_sqs_queue" "high_priority" {
   name           = "${var.app_identifier}-high-priority"
   redrive_policy = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead_letter.arn}\",\"maxReceiveCount\":10}"
+  visibility_timeout_seconds = var.sqs_visibility_timeout_seconds
 }
 
 resource "aws_sqs_queue" "default" {
   name           = "${var.app_identifier}-default"
   redrive_policy = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead_letter.arn}\",\"maxReceiveCount\":10}"
+  visibility_timeout_seconds = var.sqs_visibility_timeout_seconds
 }
 
 resource "aws_sqs_queue" "low_priority" {
   name           = "${var.app_identifier}-low-priority"
   redrive_policy = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead_letter.arn}\",\"maxReceiveCount\":10}"
+  visibility_timeout_seconds = var.sqs_visibility_timeout_seconds
 }
 
 resource "aws_sqs_queue" "scheduler" {
   name = "${var.app_identifier}-scheduler"
+  visibility_timeout_seconds = var.sqs_visibility_timeout_seconds
 }
 
 resource "aws_sqs_queue" "dead_letter" {
