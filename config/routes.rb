94aac0b44ab82dd97282337bd8fdeee3b6289a17
David Wilkie <dwilkie@gmail.com>
2022-04-02 08:38:45 +0700

    Tidy up routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,7 @@ Rails.application.routes.draw do
     devise_scope :user do
       resource(
         :registration,
-        only: %i[edit update create],
-        controller: "users/registrations",
-        as: :user_registration,
-        path: "users"
-      )
-
-      resource(
-        :registration,
-        only: %i[new],
+        only: %i[edit update new create],
         controller: "users/registrations",
         as: :user_registration,
         path: "users",
