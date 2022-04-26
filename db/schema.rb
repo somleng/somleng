35b9d71f3fba1fb91b471b25bdf1e89347a9ca76
David Wilkie <dwilkie@gmail.com>
2022-04-26 15:52:04 +0700

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_26_084655) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_26_084955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -78,10 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_26_084655) do
 
   create_table "call_data_records", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "phone_call_id", null: false
-    t.string "file_id"
-    t.string "file_filename"
-    t.integer "file_size"
-    t.string "file_content_type"
     t.integer "bill_sec", null: false
     t.integer "duration_sec", null: false
     t.string "direction", null: false
