3769ab74b738373dd2e8439c53faa36ba9586b4a
David Wilkie <dwilkie@gmail.com>
2022-05-28 08:39:35 +0700

    WIP

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store(
-  :cookie_store,
-  key: "_somleng_session",
-  domain: AppSettings.app_uri.domain
-)
+Rails.application.config.session_store(:cookie_store, key: "_somleng_session")
