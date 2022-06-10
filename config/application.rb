ab0185e4fa03e4fea7eb9ef5894ec3999b5f8a92
Samnang Chhun <samnang.chhun@gmail.com>
2022-06-10 14:01:39 +0700

    WIP

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "active_storage/engine"
 require "action_controller/railtie"
 require "action_view/railtie"
 require "action_mailer/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
