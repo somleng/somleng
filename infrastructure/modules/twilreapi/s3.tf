e0ebd1f4677e448f27303add86a013e4beba4ea8
Samnang Chhun <samnang.chhun@gmail.com>
2022-06-14 15:51:19 +0700

    WIP

--- infrastructure/modules/twilreapi/s3.tf
+++ infrastructure/modules/twilreapi/s3.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "uploads" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "POST", "PUT"]
-    allowed_origins = ["https://*.somleng.org"]
+    allowed_origins = ["https://*"]
     max_age_seconds = 3000
   }
 }
