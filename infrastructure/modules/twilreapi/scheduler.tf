eafa0f6f8aad965c4bc747d2a16464392611691f
Samnang Chhun <samnang.chhun@gmail.com>
2022-06-16 14:47:05 +0700

    Fix SQS policy

--- infrastructure/modules/twilreapi/scheduler.tf
+++ infrastructure/modules/twilreapi/scheduler.tf
@@ -1,45 +1,6 @@
-resource "aws_iam_role" "app_scheduler" {
-  name = "somleng-scheduler"
-
-  assume_role_policy = <<DOC
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "events.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-DOC
-}
-
-resource "aws_iam_role_policy" "app_scheduler" {
-  name = "somleng-scheduler"
-  role = aws_iam_role.app_scheduler.id
-
-  policy = <<DOC
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "sqs:SendMessage",
-      "Resource": "${aws_sqs_queue.scheduler.arn}"
-    }
-  ]
-}
-DOC
-}
-
 resource "aws_cloudwatch_event_rule" "scheduler_daily" {
   name                = "somleng-SchedulerJob-daily"
   schedule_expression =  "cron(0 0 * * ? *)"
-  role_arn = aws_iam_role.app_scheduler.arn
 }
 
 resource "aws_cloudwatch_event_target" "scheduler_daily" {
--- infrastructure/modules/twilreapi/sqs.tf
+++ infrastructure/modules/twilreapi/sqs.tf
@@ -20,8 +20,31 @@ resource "aws_sqs_queue" "scheduler" {
   name = "${var.app_identifier}-scheduler"
   redrive_policy = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead_letter.arn}\",\"maxReceiveCount\":10}"
   visibility_timeout_seconds = var.sqs_visibility_timeout_seconds
+
+  policy = <<DOC
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "events.amazonaws.com"
+      },
+      "Action": "sqs:SendMessage",
+      "Resource": "arn:aws:sqs:*:*:${var.app_identifier}-scheduler",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_cloudwatch_event_rule.scheduler_daily.arn}"
+        }
+      }
+    }
+  ]
+}
+DOC
 }
 
 resource "aws_sqs_queue" "dead_letter" {
   name = "${var.app_identifier}-dead-letter"
 }
+
+
