39c6cc3d6cd266054c611344e24bffb1765b12ff
Samnang Chhun <samnang.chhun@gmail.com>
2022-06-16 19:21:02 +0700

    Support recording in mp3 format

--- app/workflows/process_recording.rb
+++ app/workflows/process_recording.rb
@@ -1,3 +1,5 @@
+require "open3"
+
 class ProcessRecording < ApplicationWorkflow
   attr_reader :recording, :s3_client
 
@@ -7,23 +9,37 @@ class ProcessRecording < ApplicationWorkflow
   end
 
   def call
-    file = download_raw_recording_file
-    recording.file.attach(io: file, filename: "#{recording.id}.wav")
-    recording.file.analyze
+    Tempfile.create([recording.id, ".wav"]) do |raw_file|
+      download_raw_recording_file(raw_file)
 
-    recording.complete!
-    ExecuteWorkflowJob.perform_later(NotifyRecordingStatusCallback.to_s, recording) if recording.status_callback_url.present?
+      Tempfile.create([recording.id, ".mp3"]) do |mp3_file|
+        convert_to_mp3(raw_file, mp3_file)
+
+        recording.file.attach(io: File.open(raw_file), filename: "#{recording.id}.wav")
+        recording.mp3_file.attach(io: File.open(mp3_file), filename: "#{recording.id}.mp3")
+
+        recording.file.analyze
+        recording.mp3_file.analyze
+
+        recording.complete!
+      end
+
+      ExecuteWorkflowJob.perform_later(NotifyRecordingStatusCallback.to_s, recording) if recording.status_callback_url.present?
+    end
   end
 
   private
 
-  def download_raw_recording_file
-    raw_recording_bucket = Aws::S3::Resource.new(client: s3_client).bucket(
-      Rails.configuration.app_settings.fetch(:raw_recordings_bucket)
+  def download_raw_recording_file(output_file)
+    s3_client.get_object(
+      response_target: output_file.path,
+      bucket: Rails.configuration.app_settings.fetch(:raw_recordings_bucket),
+      key: URI(recording.raw_recording_url).path.delete_prefix("/")
     )
-    s3_object = raw_recording_bucket.object(
-      URI(recording.raw_recording_url).path.delete_prefix("/")
-    )
-    s3_object.get.body
+  end
+
+  def convert_to_mp3(raw_file, mp3_file)
+    _stdout_str, error_str, status = Open3.capture3("ffmpeg", "-y", "-i", raw_file.path, mp3_file.path)
+    raise StandardError, error_str unless status.success?
   end
 end
