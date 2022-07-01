1148b4177384b6f2ab30968aca16dc34aa954620
David Wilkie <dwilkie@gmail.com>
2022-07-01 14:59:32 +0700

    WIP

--- infrastructure/staging/main.tf
+++ infrastructure/staging/main.tf
@@ -30,9 +30,8 @@ module "twilreapi" {
   db_port = data.terraform_remote_state.core_infrastructure.outputs.db_cluster.port
   db_security_group = data.terraform_remote_state.core_infrastructure.outputs.db_security_group.id
   inbound_sip_trunks_security_group_name = "somleng-inbound-sip-trunks-staging"
-  ecs_appserver_autoscale_min_instances = 0
-  ecs_worker_autoscale_min_instances = 0
-  ecs_worker_autoscale_max_instances = 10
+  ecs_appserver_autoscale_min_instances = 1
+  ecs_worker_autoscale_min_instances = 1
 
   raw_recordings_bucket_name = "raw-recordings-staging.somleng.org"
 }
