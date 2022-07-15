56f526a95814690c181df5b49df33a84f85828a5
David Wilkie <dwilkie@gmail.com>
2022-07-15 09:22:04 +0700

    WIP

--- spec/system/dashboard/webhook_request_logs_spec.rb
+++ spec/system/dashboard/webhook_request_logs_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe "Webhook Request Logs" do
     carrier = create(:carrier)
     user = create(:user, :carrier, carrier:)
     event = create(:event, carrier:)
-    create(:webhook_request_log, event:, http_status_code: 500, failed: true)
-    create(:webhook_request_log, event:, http_status_code: 200, failed: false)
-    create(:webhook_request_log, carrier:, http_status_code: 404, failed: true)
-    create(:webhook_request_log, http_status_code: 401)
+    webhook1 = create(:webhook_request_log, event:, failed: true)
+    webhook2 = create(:webhook_request_log, event:, failed: false)
+    webhook3 = create(:webhook_request_log, carrier:, failed: true)
+    webhook4 = create(:webhook_request_log)
 
     carrier_sign_in(user)
     visit dashboard_webhook_request_logs_path(filter: { failed: true, event_id: event.id })
 
-    expect(page).to have_content("500")
-    expect(page).to have_no_content("404")
-    expect(page).to have_no_content("200")
-    expect(page).to have_no_content("401")
+    expect(page).to have_content(webhook1.id)
+    expect(page).to have_no_content(webhook2.id)
+    expect(page).to have_no_content(webhook3.id)
+    expect(page).to have_no_content(webhook4.id)
   end
 
   it "Shows a webhook request log" do
