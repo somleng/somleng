02f2592cec3ca668f8b0932ae212c929dd43582b
David Wilkie <dwilkie@gmail.com>
2022-09-30 17:48:15 +0700

    WIP

--- infrastructure/modules/twilreapi/scheduler.tf
+++ infrastructure/modules/twilreapi/scheduler.tf
@@ -3,6 +3,11 @@ resource "aws_cloudwatch_event_rule" "scheduler_daily" {
   schedule_expression =  "cron(0 0 * * ? *)"
 }
 
+resource "aws_cloudwatch_event_rule" "scheduler_hourly" {
+  name                = "somleng-${var.app_environment}-SchedulerJob-hourly"
+  schedule_expression =  "cron(0 * * * ? *)"
+}
+
 resource "aws_cloudwatch_event_target" "scheduler_daily" {
   target_id = aws_cloudwatch_event_rule.scheduler_daily.name
   arn       = aws_sqs_queue.scheduler.arn
@@ -11,6 +16,18 @@ resource "aws_cloudwatch_event_target" "scheduler_daily" {
   input = <<DOC
 {
   "job_class": "DailyJob"
+}
+DOC
+}
+
+resource "aws_cloudwatch_event_target" "scheduler_hourly" {
+  target_id = aws_cloudwatch_event_rule.scheduler_hourly.name
+  arn       = aws_sqs_queue.scheduler.arn
+  rule      = aws_cloudwatch_event_rule.scheduler_hourly.name
+
+  input = <<DOC
+{
+  "job_class": "HourlyJob"
 }
 DOC
 }
