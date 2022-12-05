80438d3716ae5c9043412e58418f334f4cd95ef7
David Wilkie <dwilkie@gmail.com>
2022-12-05 15:56:46 +0700

    Revert "Configure anycable"
    This reverts commit ab28c94e42e47482b025abab34ae84a98405c3c6.

--- config/initializers/anycable.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Add Warden middleware to AnyCable stack to allow accessing
-# Devise current user via `env["warden"].user`.
-#
-# See https://docs.anycable.io/ruby/authentication
-AnyCable::Rails::Rack.middleware.use Warden::Manager do |config|
-  Devise.warden_config = config
-end
