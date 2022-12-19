9add4d017e1f40a8779e6eb00797b9b34df4b490
David Wilkie <dwilkie@gmail.com>
2022-12-19 11:29:03 +1100

    Make credentials more dynamic

--- config/initializers/call_service.rb
+++ config/initializers/call_service.rb
@@ -1,7 +1,7 @@
 CallService.configure do |config|
-  config.host = Rails.configuration.app_settings.fetch(:ahn_host)
-  config.username = Rails.configuration.app_settings.fetch(:ahn_username)
-  config.password = Rails.configuration.app_settings.fetch(:ahn_password)
+  config.host = Rails.configuration.app_settings.fetch(:switch_host)
+  config.username = Rails.configuration.app_settings.fetch(:switch_username)
+  config.password = Rails.configuration.app_settings.fetch(:switch_password)
   config.queue_url = Rails.configuration.app_settings.fetch(:call_service_queue_url)
   config.logger = Rails.logger
   config.subscriber_realm = "somleng.org"
