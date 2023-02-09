ed84ca2e831aa1c2b9d93ee00d188ee5151169d6
David Wilkie <dwilkie@gmail.com>
2023-02-09 20:26:22 +0700

    WIP

--- spec/system/admin/carriers_spec.rb
+++ spec/system/admin/carriers_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Admin/Carriers" do
   it "List carriers" do
-    create(:carrier, name: "My Carrier")
+    create(:carrier, :with_logo, name: "My Carrier")
 
     page.driver.browser.authorize("admin", "password")
     visit admin_carriers_path
