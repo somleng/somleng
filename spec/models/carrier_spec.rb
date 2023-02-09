6b99e49675297a9364769f4fafc2007717ca743d
David Wilkie <dwilkie@gmail.com>
2023-02-09 21:47:07 +0700

    WIP

--- spec/models/carrier_spec.rb
+++ spec/models/carrier_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Carrier do
   it "handles image processing" do
     carrier = create(:carrier, :with_logo)
 
-    expect(carrier.logo.variant(resize_to_limit: [100, 100]).processed.url).to be_present
+    carrier.logo.variant(resize_to_limit: [100, 100]).processed
+
+    expect(ActiveStorage::VariantRecord.count).to eq(1)
   end
 end
