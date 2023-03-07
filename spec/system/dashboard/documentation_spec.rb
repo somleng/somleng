72d62f7745b17388bb5191041a77eade86a7424e
Samnang Chhun <samnang.chhun@gmail.com>
2023-03-07 13:30:53 +0700

    Update favicon for documentation

--- spec/system/dashboard/documentation_spec.rb
+++ spec/system/dashboard/documentation_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Documentation" do
   it "Shows carrier account API documentation" do
-    carrier = create(:carrier, :with_logo, name: "AT&T", custom_api_host: "api.att.com")
+    carrier = create(:carrier, :with_logo, :with_favicon, name: "AT&T", custom_api_host: "api.att.com")
     set_app_host(carrier)
 
     visit(docs_twilio_api_path)
@@ -10,5 +10,6 @@ RSpec.describe "Documentation" do
     expect(page).to have_css("img[alt='AT&T']")
     expect(page).to have_content("AT&T API Documentation")
     expect(page).to have_content("api.att.com")
+    expect(page).to have_css('link[rel="icon"]', visible: false)
   end
 end
