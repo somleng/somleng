eccac2e5ed5e0e2fc5a96134a523309edfe431ce
David Wilkie <dwilkie@gmail.com>
2023-07-31 19:02:44 +0700

    WIP

--- infrastructure/modules/twilreapi/lb.tf
+++ infrastructure/modules/twilreapi/lb.tf
@@ -15,7 +15,7 @@ resource "aws_lb_target_group" "webserver" {
 }
 
 resource "aws_lb_listener_rule" "webserver" {
-  priority = var.app_environment == "production" ? 17 : 117
+  priority = var.app_environment == "production" ? 15 : 115
 
   listener_arn = var.listener_arn
 
@@ -36,7 +36,7 @@ resource "aws_lb_listener_rule" "webserver" {
 }
 
 resource "aws_lb_target_group" "old_webserver" {
-  name = "${var.old_app_identifier}"
+  name = "${var.old_service_name}"
   port = 80
   protocol = "HTTP"
   vpc_id = var.vpc.vpc_id
