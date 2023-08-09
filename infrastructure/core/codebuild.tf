62a4b73837d0b5865548b6dd7bfa829e04970d34
David Wilkie <dwilkie@gmail.com>
2023-08-09 12:45:14 +0700

    Setup codedeploy

---
+++ infrastructure/core/codebuild.tf
@@ -0,0 +1,69 @@
+locals {
+  codebuild_project_name = "somleng"
+}
+
+data "aws_iam_policy_document" "codebuild_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "codebuild" {
+  name               = "codebuild-${local.codebuild_project_name}"
+  assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role.json
+}
+
+data "aws_iam_policy_document" "codebuild" {
+  statement {
+    effect    = "Allow"
+
+    resources = [
+      "arn:aws:logs:::log-group:/aws/codebuild/${local.codebuild_project_name}",
+      "arn:aws:logs:::log-group:/aws/codebuild/${local.codebuild_project_name}:*"
+    ]
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "codebuild" {
+  role   = aws_iam_role.codebuild.name
+  policy = data.aws_iam_policy_document.codebuild.json
+}
+
+resource "aws_codebuild_project" "this" {
+  name           = local.codebuild_project_name
+
+  service_role = aws_iam_role.codebuild.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  cache {
+    type  = "LOCAL"
+    modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-aarch64-standard:3.0"
+    type                        = "ARM_CONTAINER"
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/somleng/somleng.git"
+  }
+}
--- infrastructure/core/variables.tf
+++ infrastructure/core/variables.tf
@@ -1,3 +1,7 @@
 variable "aws_region" {
   default = "ap-southeast-1"
 }
+
+locals {
+  vpc = data.terraform_remote_state.core_infrastructure.outputs.vpc
+}
