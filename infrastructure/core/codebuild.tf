ab59a9813508d55cc340f594cb70238488123d21
David Wilkie <dwilkie@gmail.com>
2023-08-09 13:34:38 +0700

    WIP

--- infrastructure/core/codebuild.tf
+++ infrastructure/core/codebuild.tf
@@ -61,6 +61,11 @@ resource "aws_codebuild_project" "this" {
     image = "aws/codebuild/amazonlinux2-aarch64-standard:3.0"
     type = "ARM_CONTAINER"
     privileged_mode = true
+
+    environment_variable {
+      name  = "AWS_DEFAULT_REGION"
+      value = var.aws_region
+    }
   }
 
   source {
