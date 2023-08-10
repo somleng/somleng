59e7bee6ec4fccad1f10b4dd515d1c717f21ead7
David Wilkie <dwilkie@gmail.com>
2023-08-10 16:16:49 +0700

    WIP

--- infrastructure/core/codebuild.tf
+++ infrastructure/core/codebuild.tf
@@ -47,6 +47,32 @@ resource "aws_iam_role_policy_attachment" "codebuild_ecr_public" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonElasticContainerRegistryPublicPowerUser"
 }
 
+resource "aws_codebuild_project" "amd64" {
+  name           = "${local.codebuild_identifier}-amd64"
+
+  service_role = aws_iam_role.codebuild.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  cache {
+    type  = "LOCAL"
+    modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+    type = "LINUX_CONTAINER"
+    privileged_mode = true
+  }
+
+  source {
+    type = "GITHUB"
+    location = "https://github.com/somleng/somleng.git"
+  }
+}
 
 resource "aws_codebuild_project" "arm64" {
   name           = "${local.codebuild_identifier}-arm64"
@@ -67,11 +93,6 @@ resource "aws_codebuild_project" "arm64" {
     image = "aws/codebuild/amazonlinux2-aarch64-standard:3.0"
     type = "ARM_CONTAINER"
     privileged_mode = true
-
-    environment_variable {
-      name  = "AWS_DEFAULT_REGION"
-      value = var.aws_region
-    }
   }
 
   source {
