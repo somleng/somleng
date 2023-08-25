a8bbd8a23d0da88ef142982a1003bd569c979d90
David Wilkie <dwilkie@gmail.com>
2023-08-25 15:12:43 +0700

    Switch to ECR private (#1050)

--- infrastructure/core/outputs.tf
+++ infrastructure/core/outputs.tf
@@ -1,9 +1,9 @@
 output "app_ecr_repository" {
-  value = aws_ecrpublic_repository.app.repository_uri
+  value = aws_ecr_repository.app.repository_url
 }
 
 output "nginx_ecr_repository" {
-  value = aws_ecrpublic_repository.nginx.repository_uri
+  value = aws_ecr_repository.nginx.repository_url
 }
 
 output "elasticache_redis_endpoint" {
