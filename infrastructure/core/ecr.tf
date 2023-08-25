69077f6b44a7fab5bf2e375410a2f199e6df8832
David Wilkie <dwilkie@gmail.com>
2023-08-25 13:34:20 +0700

    Switch to ECR Private

--- infrastructure/core/ecr.tf
+++ infrastructure/core/ecr.tf
@@ -1,20 +1,17 @@
-resource "aws_ecrpublic_repository" "app" {
-  repository_name = "somleng"
-  provider = aws.us-east-1
+resource "aws_ecr_repository" "app" {
+  name                 = "somleng"
+  image_tag_mutability = "MUTABLE"
 
-  catalog_data {
-    about_text        = "Somleng"
-    architectures     = ["Linux"]
-    description       = "Somleng is an Open Source Cloud Communications Platform as a Service (CPaaS). It contains an open source implementation of Twilio's REST API, as well as functionality for carrier services."
+  image_scanning_configuration {
+    scan_on_push = true
   }
 }
 
-resource "aws_ecrpublic_repository" "nginx" {
-  repository_name = "somleng-nginx"
-  provider = aws.us-east-1
+resource "aws_ecr_repository" "nginx" {
+  name                 = "somleng-nginx"
+  image_tag_mutability = "MUTABLE"
 
-  catalog_data {
-    about_text        = "Somleng Nginx"
-    architectures     = ["Linux"]
+  image_scanning_configuration {
+    scan_on_push = true
   }
 }
