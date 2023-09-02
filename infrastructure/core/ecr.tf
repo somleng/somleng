d3eb91c98e4923a1c2d19b12cf153ba066113e62
David Wilkie <dwilkie@gmail.com>
2023-09-02 15:07:16 +0700

    Setup ECR lifecycle policy

--- infrastructure/core/ecr.tf
+++ infrastructure/core/ecr.tf
@@ -15,3 +15,103 @@ resource "aws_ecr_repository" "nginx" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Expire untagged images",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 1
+      },
+      "action": {
+        "type": "expire"
+      }
+    },
+    {
+      "rulePriority": 2,
+      "description": "Expire old production images",
+      "selection": {
+        "tagStatus": "tagged",
+        "tagPrefixList": ["prod"],
+        "countType": "imageCountMoreThan",
+        "countNumber": 5
+      },
+      "action": {
+        "type": "expire"
+      }
+    },
+    {
+      "rulePriority": 3,
+      "description": "Expire old staging images",
+      "selection": {
+        "tagStatus": "tagged",
+        "tagPrefixList": ["stag"],
+        "countType": "imageCountMoreThan",
+        "countNumber": 5
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "nginx" {
+  repository = aws_ecr_repository.nginx.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Expire untagged images",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 1
+      },
+      "action": {
+        "type": "expire"
+      }
+    },
+    {
+      "rulePriority": 2,
+      "description": "Expire old production images",
+      "selection": {
+        "tagStatus": "tagged",
+        "tagPrefixList": ["prod"],
+        "countType": "imageCountMoreThan",
+        "countNumber": 5
+      },
+      "action": {
+        "type": "expire"
+      }
+    },
+    {
+      "rulePriority": 3,
+      "description": "Expire old staging images",
+      "selection": {
+        "tagStatus": "tagged",
+        "tagPrefixList": ["stag"],
+        "countType": "imageCountMoreThan",
+        "countNumber": 5
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
