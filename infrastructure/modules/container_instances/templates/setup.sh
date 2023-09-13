112d223f6c9a94759ace887c6386f7dadf1b4d5c
Samnang Chhun <samnang.chhun@gmail.com>
2023-09-13 13:55:34 +0700

    WIP

--- infrastructure/modules/container_instances/templates/setup.sh
+++ infrastructure/modules/container_instances/templates/setup.sh
@@ -8,7 +8,4 @@ cat <<'EOF' >>/etc/ecs/ecs.config
 ECS_CLUSTER=${cluster_name}
 ECS_RESERVED_MEMORY=256
 ECS_ENABLE_CONTAINER_METADATA=true
-ECS_LOGLEVEL=debug
-ECS_LOG_OUTPUT_FORMAT=json
-ECS_LOG_DRIVER=awslogs
 EOF
