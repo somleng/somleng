23f490b160ecfc36987ff3c144db095a1aab342d
Samnang Chhun <samnang.chhun@gmail.com>
2023-09-13 11:39:04 +0700

    WIP

--- infrastructure/modules/container_instances/templates/setup.sh
+++ infrastructure/modules/container_instances/templates/setup.sh
@@ -4,8 +4,10 @@ systemctl enable amazon-ssm-agent
 systemctl start amazon-ssm-agent
 
 # ECS config
-cat <<'EOF' >> /etc/ecs/ecs.config
+cat <<'EOF' >>/etc/ecs/ecs.config
 ECS_CLUSTER=${cluster_name}
 ECS_RESERVED_MEMORY=256
 ECS_ENABLE_CONTAINER_METADATA=true
+ECS_LOGLEVEL=debug
+ECS_LOG_OUTPUT_FORMAT=json
 EOF
