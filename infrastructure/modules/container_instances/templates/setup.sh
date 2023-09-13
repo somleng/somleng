4b89078ab6d8eba1c67b372bc1b2442c80221e5f
David Wilkie <dwilkie@gmail.com>
2023-09-13 14:30:18 +0700

    Fix worker memory issues (#1077)

--- infrastructure/modules/container_instances/templates/setup.sh
+++ infrastructure/modules/container_instances/templates/setup.sh
@@ -4,7 +4,7 @@ systemctl enable amazon-ssm-agent
 systemctl start amazon-ssm-agent
 
 # ECS config
-cat <<'EOF' >> /etc/ecs/ecs.config
+cat <<'EOF' >>/etc/ecs/ecs.config
 ECS_CLUSTER=${cluster_name}
 ECS_RESERVED_MEMORY=256
 ECS_ENABLE_CONTAINER_METADATA=true
