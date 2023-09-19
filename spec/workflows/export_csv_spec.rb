97998cfdc284e28d47a370f522ae7f4d4d786709
David Wilkie <dwilkie@gmail.com>
2023-09-19 09:01:32 +0700

    Fix CSV memory (#1089)

---
+++ spec/workflows/export_csv_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe ExportCSV do
+  it "exports a CSV" do
+    carrier = create(:carrier)
+    user = create(:user, :carrier, carrier:)
+    export = create(
+      :export,
+      resource_type: "PhoneCall",
+      user:,
+      scoped_to: { carrier_id: carrier.id }
+    )
+
+    phone_call = create(:phone_call, carrier:)
+    _other_phone_call = create(:phone_call)
+
+    ExportCSV.call(export)
+
+    expect(export.file.attached?).to eq(true)
+    expect(export.file.blob.filename).to eq(export.name)
+    expect(export.file.blob.content_type).to eq("text/csv")
+    csv_data = CSV.parse(export.file.download, headers: true)
+    expect(csv_data["sid"]).to eq([phone_call.id])
+  end
+end
