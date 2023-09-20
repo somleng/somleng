55c65f402e83b003a8cb9b911c757ac20b38df4f
David Wilkie <dwilkie@gmail.com>
2023-09-20 08:50:10 +0700

    WIP

--- app/dashboards/import_dashboard.rb
+++ app/dashboards/import_dashboard.rb
@@ -1,13 +1,5 @@
 require "administrate/base_dashboard"
 
-# t.string "resource_type", null: false
-# t.string "status", null: false
-# t.string "error_message"
-# t.uuid "user_id", null: false
-# t.uuid "carrier_id", null: false
-# t.datetime "created_at", null: false
-# t.datetime "updated_at", null: false
-
 class ImportDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     id: Field::String,
