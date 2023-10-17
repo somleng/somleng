3e40b08695b64e1a1cb4d164fe115b1a910deecf
David Wilkie <dwilkie@gmail.com>
2023-10-17 11:13:23 +0700

    WIP

--- app/models/account.rb
+++ app/models/account.rb
@@ -21,6 +21,7 @@ class Account < ApplicationRecord
   has_many :recordings
   has_many :error_logs
   has_many :interactions
+  has_many :tts_events
 
   def self.customer_managed
     where(arel_table[:account_memberships_count].gt(0))
