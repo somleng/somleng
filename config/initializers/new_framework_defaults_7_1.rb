01fb2b07e61fb18ffbdcaeafc606a6b3c167e365
David Wilkie <dwilkie@gmail.com>
2023-10-21 20:23:31 +0700

    Upgrade activerecord encryption

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -34,7 +34,7 @@
 #
 # 1. If you have +config.active_support.key_generator_hash_digest_class+ configured as SHA1 (the default
 # before Rails 7.0), you need to configure SHA-1 for Active Record Encryption too:
-Rails.application.config.active_record.encryption.hash_digest_class = OpenSSL::Digest::SHA1
+# Rails.application.config.active_record.encryption.hash_digest_class = OpenSSL::Digest::SHA1
 # 2. If you have +config.active_support.key_generator_hash_digest_class+ configured as SHA256 (the new default
 # in 7.0), then you need to configure SHA-256 for Active Record Encryption:
 # Rails.application.config.active_record.encryption.hash_digest_class = OpenSSL::Digest::SHA256
