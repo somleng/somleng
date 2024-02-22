595260c9bb837d2b537326aee79f88838019259b
David Wilkie <dwilkie@gmail.com>
2024-02-22 16:23:35 +0700

    WIP

--- spec/mailers/previews/error_log_mailer_preview.rb
+++ spec/mailers/previews/error_log_mailer_preview.rb
@@ -1,18 +1,18 @@
 class ErrorLogMailerPreview < ActionMailer::Preview
   def notify_carrier_user
-    error_log_notification = ErrorLogNotification
-                            .joins(:error_log)
-                            .where.not(error_logs: { carrier_id: nil })
-                            .where(error_logs: { account_id: nil }).last
+    error_log = ErrorLog
+               .joins(:notifications)
+               .where.not(carrier_id: nil)
+               .where(account_id: nil).last
 
-    ErrorLogMailer.notify(error_log: error_log_notification.error_log)
+    ErrorLogMailer.notify(error_log:)
   end
 
   def notify_account_user
-    error_log_notification = ErrorLogNotification
-                            .joins(error_log: :account)
-                            .merge(Account.customer_managed).last
+    error_log = ErrorLog.joins(:notifications)
+                        .joins(:account)
+                        .merge(Account.customer_managed).last
 
-    ErrorLogMailer.notify(error_log: error_log_notification.error_log)
+    ErrorLogMailer.notify(error_log:)
   end
 end
