e024df153ef20b1765a2dd416a9b555190ffe0d9
David Wilkie <dwilkie@gmail.com>
2024-07-11 01:18:40 +0700

    WIP

--- db/migrate/20240625080755_remove_call_leg_from_call_data_records.rb
+++ db/migrate/20240625080755_remove_call_leg_from_call_data_records.rb
@@ -1,5 +1,14 @@
 class RemoveCallLegFromCallDataRecords < ActiveRecord::Migration[7.1]
   def change
+    reversible do |dir|
+      dir.up do
+        CallDataRecord.where(call_leg: "B").delete_all
+
+        duplicate_cdrs = CallDataRecord.where(phone_call_id: CallDataRecord.select(:phone_call_id).group(:phone_call_id).having("count(phone_call_id) > 1"))
+        duplicate_cdrs.where.not(id: duplicate_cdrs.select("DISTINCT ON(phone_call_id) id")).delete_all
+      end
+    end
+
     remove_column(:call_data_records, :call_leg, :string)
     remove_index(:call_data_records, :phone_call_id)
     add_index(:call_data_records, :phone_call_id, unique: true)
