f15eda8cd9e87fff6bb2e789e1b6049600e3d04b
David Wilkie <dwilkie@gmail.com>
2024-09-02 08:50:31 +0700

    Fix SMS Gateway resolving for verifications

--- spec/request_schemas/twilio_api/verify/verification_request_schema_spec.rb
+++ spec/request_schemas/twilio_api/verify/verification_request_schema_spec.rb
@@ -271,7 +271,9 @@ module TwilioAPI
       def create_verification_service(attributes = {})
         verification_service = create(:verification_service, attributes)
         phone_number = create(:phone_number, carrier: verification_service.carrier)
-        create(:sms_gateway, carrier: verification_service.carrier, default_sender: phone_number)
+        sms_gateway = create(:sms_gateway, carrier: verification_service.carrier, default_sender: phone_number)
+        channel_group = create(:sms_gateway_channel_group, sms_gateway:, route_prefixes: [])
+        create(:sms_gateway_channel, slot_index: 1, channel_group:, sms_gateway:)
         create(:sip_trunk, carrier: verification_service.carrier, default_sender: phone_number)
         [ verification_service, phone_number ]
       end
