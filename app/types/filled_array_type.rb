c8a0430b639025d686e389192b9a14f0d03ee6a3
David Wilkie <dwilkie@gmail.com>
2024-10-15 08:36:38 +0700

    WIP

--- app/types/filled_array_type.rb
+++ app/types/filled_array_type.rb
@@ -1,14 +1,5 @@
 class FilledArrayType < ActiveRecord::Type::String
-  attr_reader :unique
-
-  def initialize(**options)
-    @unique = options.delete(:unique) || false
-    super
-  end
-
   def cast(value)
-    result = Array(value).reject(&:blank?)
-    result = result.uniq if unique
-    result
+    Array(value).reject(&:blank?)
   end
 end
--- db/migrate/20241006052838_remove_inbound_source_ip_from_sip_trunks.rb
+++ db/migrate/20241006052838_remove_inbound_source_ip_from_sip_trunks.rb
@@ -4,6 +4,7 @@ class RemoveInboundSourceIPFromSIPTrunks < ActiveRecord::Migration[7.2]
       dir.up do
         SIPTrunk.where.not(inbound_source_ip: nil).find_each do |sip_trunk|
           sip_trunk.inbound_source_ips = sip_trunk.inbound_source_ip
+          sip_trunk.save!
         end
 
         remove_column(:sip_trunks, :inbound_source_ip, :inet)
@@ -12,7 +13,7 @@ class RemoveInboundSourceIPFromSIPTrunks < ActiveRecord::Migration[7.2]
       dir.down do
         add_column(:sip_trunks, :inbound_source_ip, :inet)
 
-        SIPTrunk.find_each do |sip_trunk|
+        SIPTrunk.joins(:sip_trunk_inbound_source_ip_addresses).find_each do |sip_trunk|
           sip_trunk_inbound_source_ip_address = sip_trunk.sip_trunk_inbound_source_ip_addresses.first
           sip_trunk.update_columns(inbound_source_ip: sip_trunk_inbound_source_ip_address.ip)
         end
