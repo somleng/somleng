cca2be6e8760549a001555666fbef2784e2b2504
David Wilkie <dwilkie@gmail.com>
2024-10-15 11:47:30 +0700

    Cleanup SIP Trunk Permissions PR

--- db/migrate/20241006052838_remove_inbound_source_ip_from_sip_trunks.rb
+++ db/migrate/20241006052838_remove_inbound_source_ip_from_sip_trunks.rb
@@ -1,33 +1,5 @@
 class RemoveInboundSourceIPFromSIPTrunks < ActiveRecord::Migration[7.2]
   def change
-    reversible do |dir|
-      dir.up do
-        SIPTrunk.where.not(inbound_source_ip: nil).find_each do |sip_trunk|
-          sip_trunk.inbound_source_ips = sip_trunk.inbound_source_ip
-          sip_trunk.save!
-          sip_trunk.sip_trunk_inbound_source_ip_addresses.update_all(
-            created_at: sip_trunk.updated_at,
-            updated_at: sip_trunk.updated_at
-          )
-          sip_trunk.inbound_source_ip_addresses.update_all(
-            created_at: sip_trunk.updated_at,
-            updated_at: sip_trunk.updated_at
-          )
-        end
-
-        raise unless InboundSourceIPAddress.count == SIPTrunk.pluck(:inbound_source_ip).count
-
-        remove_column(:sip_trunks, :inbound_source_ip, :inet)
-      end
-
-      dir.down do
-        add_column(:sip_trunks, :inbound_source_ip, :inet)
-
-        SIPTrunk.joins(:sip_trunk_inbound_source_ip_addresses).find_each do |sip_trunk|
-          sip_trunk_inbound_source_ip_address = sip_trunk.sip_trunk_inbound_source_ip_addresses.first
-          sip_trunk.update_columns(inbound_source_ip: sip_trunk_inbound_source_ip_address.ip)
-        end
-      end
-    end
+    remove_column(:sip_trunks, :inbound_source_ip, :inet)
   end
 end
