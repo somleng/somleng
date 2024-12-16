216b7e7757f78852ec334b46f5fa014a6ece2314
David Wilkie <dwilkie@gmail.com>
2024-12-16 11:08:46 +1100

    Fix incoming phone numbers form (#1782)

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
-Aws::Rails.add_action_mailer_delivery_method(
+ActionMailer::Base.add_delivery_method(
   :ses, region: Rails.configuration.app_settings.fetch(:aws_ses_region)
 )
 Aws.config.update(log_level: :warn)
