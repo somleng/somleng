9ffbb8a48df91a9302ceffc1140e6a9158ac57a4
David Wilkie <dwilkie@gmail.com>
2025-06-16 15:01:53 +0700

    Handle session limit when expiring phone calls

--- app/workflows/expire_in_progress_phone_calls.rb
+++ app/workflows/expire_in_progress_phone_calls.rb
@@ -1,5 +1,15 @@
 class ExpireInProgressPhoneCalls < ApplicationWorkflow
+  attr_reader :session_limiters
+
+  def initialize(**options)
+    super()
+    @session_limiters = options.fetch(:session_limiters) { [ AccountCallSessionLimiter.new, GlobalCallSessionLimiter.new ] }
+  end
+
   def call
-    PhoneCall.in_progress.where(initiated_at: ..4.hours.ago).update_all(status: :session_timeout)
+    PhoneCall.in_progress.where(initiated_at: ..4.hours.ago).find_each do |phone_call|
+      phone_call.update_columns(status: :session_timeout)
+      session_limiters.each { _1.remove_session_from(phone_call.region.alias, scope: phone_call.account_id) }
+    end
   end
 end
--- spec/workflows/expire_in_progress_phone_calls_spec.rb
+++ spec/workflows/expire_in_progress_phone_calls_spec.rb
@@ -2,12 +2,28 @@ require "rails_helper"
 
 RSpec.describe ExpireInProgressPhoneCalls do
   it "expires in progress phone calls" do
-    to_be_expired_phone_call = create(:phone_call, :initiated, initiated_at: 4.hours.ago)
-    phone_call = create(:phone_call, :initiated, initiated_at: 3.hours.ago)
+    account = create(:account)
+    to_be_expired_phone_call = create(:phone_call, :initiated, account:, region: :hydrogen, initiated_at: 4.hours.ago)
+    phone_call = create(:phone_call, :initiated, account:, region: :hydrogen, initiated_at: 3.hours.ago)
+    account_session_limiter, global_session_limiter = build_session_limiters(account:, sessions: { hydrogen: 1 }, limit: 1)
 
-    ExpireInProgressPhoneCalls.call
+    ExpireInProgressPhoneCalls.call(session_limiters: [ account_session_limiter, global_session_limiter ])
 
-    expect(to_be_expired_phone_call.reload.session_timeout?).to eq(true)
-    expect(phone_call.reload.session_timeout?).to eq(false)
+    expect(to_be_expired_phone_call.reload.session_timeout?).to be(true)
+    expect(phone_call.reload.session_timeout?).to be(false)
+    expect(account_session_limiter.session_count_for(:hydrogen, scope: account.id)).to eq(0)
+    expect(global_session_limiter.session_count_for(:hydrogen)).to eq(0)
+  end
+
+  def build_session_limiters(account:, sessions: {}, **)
+    session_limiters = [ AccountCallSessionLimiter.new(**), GlobalCallSessionLimiter.new(**) ]
+
+    sessions.each do |region, count|
+      session_limiters.each do |session_limiter|
+        count.times { session_limiter.add_session_to(region, scope: account.id) }
+      end
+    end
+
+    session_limiters
   end
 end
