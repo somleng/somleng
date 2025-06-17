0dc54b64bf626699d108cc00fc118c0fd804aaaa
David Wilkie <dwilkie@gmail.com>
2025-06-17 17:45:58 +0700

    Better handling of enqueued calls

--- spec/workflows/process_outbound_calls_queue_spec.rb
+++ spec/workflows/process_outbound_calls_queue_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe ProcessOutboundCallsQueue do
-  it "enqueues outbound call jobs" do
+  it "enqueues a job to process each call" do
     accounts = create_list(:account, 2)
     accounts.each do |account|
       2.times { OutboundCallsQueue.new(account).enqueue(SecureRandom.uuid) }
@@ -10,7 +10,16 @@ RSpec.describe ProcessOutboundCallsQueue do
     ProcessOutboundCallsQueue.call
 
     accounts.each do |account|
-      expect(OutboundCallJob).to have_been_enqueued.with(account).exactly(1).times
+      expect(OutboundCallJob).to have_been_enqueued.with(account).exactly(2).times
     end
+  end
+
+  it "enqueues a maximum of 60 jobs per account" do
+    account = create(:account)
+    100.times { OutboundCallsQueue.new(account).enqueue(SecureRandom.uuid) }
+
+    ProcessOutboundCallsQueue.call
+
+    expect(OutboundCallJob).to have_been_enqueued.with(account).exactly(60).times
   end
 end
