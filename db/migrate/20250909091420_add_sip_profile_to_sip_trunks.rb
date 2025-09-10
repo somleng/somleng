13f1ead681743601ea95136ba35b29dbcdff5044
David Wilkie <dwilkie@gmail.com>
2025-09-10 18:39:15 +0700

    Cleanup SIP profiles PR (#2158)

--- db/migrate/20250909091420_add_sip_profile_to_sip_trunks.rb
+++ db/migrate/20250909091420_add_sip_profile_to_sip_trunks.rb
@@ -1,16 +1,7 @@
 class AddSIPProfileToSIPTrunks < ActiveRecord::Migration[8.0]
   def change
-    add_column :sip_trunks, :sip_profile, :string
+    add_column :sip_trunks, :sip_profile, :string, null: false
     add_index :sip_trunks, :sip_profile
-
-    reversible do |dir|
-      dir.up do
-        SIPTrunk.where(outbound_symmetric_latching_supported: false).update_all(sip_profile: :nat_instance)
-        SIPTrunk.where(outbound_symmetric_latching_supported: true).update_all(sip_profile: :nat_gateway)
-      end
-    end
-
-    change_column_null :sip_trunks, :sip_profile, false
     remove_column :sip_trunks, :outbound_symmetric_latching_supported, :boolean, null: false, default: true
   end
 end
