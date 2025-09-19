dd7b692258bf36f7dcbe1e3f098807c45819ecc3
David Wilkie <dwilkie@gmail.com>
2025-09-19 16:47:15 +0700

    Publish call queue stats even when empty (#2175)

--- infrastructure/modules/somleng/cloudwatch.tf
+++ infrastructure/modules/somleng/cloudwatch.tf
@@ -34,7 +34,26 @@ resource "aws_cloudwatch_log_metric_filter" "outbound_calls_queue" {
     value     = "$.outbound_calls_queue_metrics.count"
     unit      = "Count"
     dimensions = {
-      Queue = "$.outbound_calls_queue_metrics.queue"
+      Region = "$.outbound_calls_queue_metrics.queue"
+    }
+  }
+
+  depends_on = [null_resource.log_transformer]
+}
+
+resource "aws_cloudwatch_log_metric_filter" "call_service_capacity" {
+  name                      = "${var.app_identifier}-CallServiceCapacity"
+  log_group_name            = aws_cloudwatch_log_group.app.name
+  pattern                   = "{ $.call_service_capacity.capacity = * }"
+  apply_on_transformed_logs = true
+
+  metric_transformation {
+    name      = "${var.app_identifier}-CallServiceCapacity"
+    namespace = "Somleng"
+    value     = "$.call_service_capacity.capacity"
+    unit      = "Count"
+    dimensions = {
+      Region = "$.call_service_capacity.region"
     }
   }
 
--- spec/workflows/publish_outbound_calls_queue_metrics_spec.rb
+++ spec/workflows/publish_outbound_calls_queue_metrics_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe PublishOutboundCallsQueueMetrics do
   it "publishes metrics about the queue" do
     create_list(:phone_call, 3, :queued, region: :hydrogen)
-    create_list(:phone_call, 2, :queued, region: :helium)
     create(:phone_call, :completed, region: :hydrogen)
     CallServiceCapacity.set_for("hydrogen", capacity: 5)
     CallServiceCapacity.set_for("helium", capacity: 2)
@@ -12,7 +11,7 @@ RSpec.describe PublishOutboundCallsQueueMetrics do
     PublishOutboundCallsQueueMetrics.call(logger:)
 
     assert_log_json(logger, outbound_calls_queue_metrics: { queue: "hydrogen", count: 3 })
-    assert_log_json(logger, outbound_calls_queue_metrics: { queue: "helium", count: 2 })
+    assert_log_json(logger, outbound_calls_queue_metrics: { queue: "helium", count: 0 })
     assert_log_json(logger, call_service_capacity: { region: "hydrogen", capacity: 5 })
     assert_log_json(logger, call_service_capacity: { region: "helium", capacity: 2 })
   end
