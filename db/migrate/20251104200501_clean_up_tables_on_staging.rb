157fc74a3d4c5e3d2c229225038d7b292316be4d
Samnang Chhun <samnang.chhun@gmail.com>
2025-11-04 21:29:04 +0700

    Remove device type column from migration

--- db/migrate/20251104200501_clean_up_tables_on_staging.rb
+++ db/migrate/20251104200501_clean_up_tables_on_staging.rb
@@ -1,6 +1,7 @@
 # TODO: Delete this migration after staging is deployed
 class CleanUpTablesOnStaging < ActiveRecord::Migration[8.1]
   def change
+    remove_column :sms_gateways, :device_type, if_exists: true
     drop_table :action_push_native_devices, if_exists: true
     drop_table :message_send_requests, if_exists: true
   end
