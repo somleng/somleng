cd3271435bb93b4a55b2d4e71348529cc4094664
David Wilkie <dwilkie@gmail.com>
2026-01-13 13:42:50 +0700

    Cleanup add billing mode to accounts migration

--- db/migrate/20260113055439_add_billing_enabled_and_billing_mode_to_accounts.rb
+++ db/migrate/20260113055439_add_billing_enabled_and_billing_mode_to_accounts.rb
@@ -1,15 +1,7 @@
 class AddBillingEnabledAndBillingModeToAccounts < ActiveRecord::Migration[8.1]
   def change
     add_column(:accounts, :billing_enabled, :boolean, null: false, default: false)
-    add_column(:accounts, :billing_mode, :string)
+    add_column(:accounts, :billing_mode, :string, null: false)
     add_index(:accounts, :billing_enabled)
-
-    reversible do |dir|
-      dir.up do
-        Account.update_all(billing_mode: :prepaid)
-      end
-    end
-
-    change_column_null(:accounts, :billing_mode, false)
   end
 end
