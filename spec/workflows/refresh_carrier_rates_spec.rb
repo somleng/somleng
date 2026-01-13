35733472ebf695a59db6907a8a715cbd29bbba7c
David Wilkie <dwilkie@gmail.com>
2026-01-13 14:28:22 +0700

    WIP

--- spec/workflows/refresh_carrier_rates_spec.rb
+++ spec/workflows/refresh_carrier_rates_spec.rb
@@ -3,15 +3,14 @@ require "rails_helper"
 RSpec.describe RefreshCarrierRates do
   it "refreshes the rates for all carriers" do
     carrier = create(:carrier, :billing_enabled)
-    create(:account, :billing_enabled, carrier:)
-    create(:account, :billing_enabled, carrier:)
+    create_list(:account, 2, :billing_enabled, carrier:)
     create(:account, :billing_enabled, carrier: create(:carrier, billing_enabled: false))
     create(:account, billing_enabled: false, carrier: create(:carrier, :billing_enabled))
     client = instance_spy(RatingEngineClient)
 
     RefreshCarrierRates.call(client:)
 
-    expect(client).to have_received(:refresh_carrier_rates).once
     expect(client).to have_received(:refresh_carrier_rates).with(carrier)
+    expect(client).to have_received(:refresh_carrier_rates).once
   end
 end
