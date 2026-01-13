defa380c8d74bfe61552b18476b903cfa4425b64
David Wilkie <dwilkie@gmail.com>
2026-01-13 14:33:10 +0700

    Cleanup billing enabled migration (#2395)

--- spec/factories.rb
+++ spec/factories.rb
@@ -68,6 +68,10 @@ FactoryBot.define do
     website { "https://at-t.com" }
     with_oauth_application
 
+    trait :billing_enabled do
+      billing_enabled { true }
+    end
+
     trait :restricted do
       restricted { true }
     end
