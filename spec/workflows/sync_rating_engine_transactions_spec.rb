3ad27447c4be69a264df7208b1d39b8265d4fb50
David Wilkie <dwilkie@gmail.com>
2026-01-27 19:30:35 +0700

    Cleanup spec

--- spec/workflows/sync_rating_engine_transactions_spec.rb
+++ spec/workflows/sync_rating_engine_transactions_spec.rb
@@ -5,62 +5,35 @@ RSpec.describe SyncRatingEngineTransactions do
     carrier = create(:carrier)
     account = create(:account, carrier:)
     message = create(:message, :inbound, account:)
-    topup_balance_transaction = create(:balance_transaction, :topup, account:)
+    existing_balance_transaction = create(:balance_transaction, :topup, account:)
+    cdrs = [
+      build_cdr(id: 1000, account_id: account.id, origin_id: message.id, category: message.tariff_category, cost: 100),
+      build_cdr(id: 1001, account_id: account.id, balance_transaction_id: existing_balance_transaction.id),
+      build_cdr(id: 1002, account_id: account.id, success?: false)
+    ]
 
-    client = instance_double(
-      RatingEngineClient, fetch_cdrs: [
-        build_cdr(id: 1000, account_id: account.id, origin_id: message.id, category: message.tariff_category, cost: 100),
-        build_cdr(id: 1001, account_id: account.id, balance_transaction_id: topup_balance_transaction.id),
-        build_cdr(id: 1002, account_id: account.id, success?: false)
-      ]
-    )
+    client = instance_double(RatingEngineClient, fetch_cdrs: cdrs)
 
-    perform_enqueued_jobs do
-      SyncRatingEngineTransactions.call(client:)
-    end
+    SyncRatingEngineTransactions.call(client:)
 
-    expect(carrier.balance_transactions).to contain_exactly(
-      have_attributes(
-        external_id: 1000,
-        account:,
-        carrier:,
-        type: "charge",
-        amount: InfinitePrecisionMoney.new(-100, account.billing_currency),
-        charge_category: "inbound_messages",
-        message: have_attributes(
-          price: InfinitePrecisionMoney.new(100, account.billing_currency)
-        ),
-      ),
-      have_attributes(
-        external_id: 1001,
-        account:,
-        carrier:,
-        type: "topup",
-      ),
+    expect(ProcessRatingEngineCDRJob).to have_been_enqueued.once
+    expect(ProcessRatingEngineCDRJob).to have_been_enqueued.with(cdrs[0].to_h).once
+    expect(existing_balance_transaction.reload).to have_attributes(
+      external_id: 1001
     )
   end
 
   it "syncs rating engine transactions with pagination" do
     client = instance_double(RatingEngineClient)
-    account = create(:account)
     allow(client).to receive(:fetch_cdrs).with(last_id: nil, limit: 2).and_return(
-      [
-        build_cdr(id: 1000, account_id: account.id),
-        build_cdr(id: 1001, account_id: account.id)
-      ]
+      [ build_cdr(id: 1000), build_cdr(id: 1001) ]
     )
-
     allow(client).to receive(:fetch_cdrs).with(last_id: 1001, limit: 2).and_return(
-      [
-        build_cdr(id: 1001, account_id: account.id)
-      ]
+      [ build_cdr(id: 1001) ]
     )
 
-    perform_enqueued_jobs do
-      SyncRatingEngineTransactions.call(client:, batch_size: 2)
-    end
+    SyncRatingEngineTransactions.call(client:, batch_size: 2)
 
-    expect(BalanceTransaction.count).to eq(2)
     expect(client).to have_received(:fetch_cdrs).twice
   end
 
