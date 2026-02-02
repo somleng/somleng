79f740ec89dcd8a9ec2d6173eabef3d4f38e1882
David Wilkie <dwilkie@gmail.com>
2026-02-02 14:05:48 +0700

    WIP

--- db/migrate/20260126112656_rename_price_to_price_cents_on_messages_and_phone_calls.rb
+++ db/migrate/20260126112656_rename_price_to_price_cents_on_messages_and_phone_calls.rb
@@ -5,6 +5,12 @@ class RenamePriceToPriceCentsOnMessagesAndPhoneCalls < ActiveRecord::Migration[8
     rename_column :messages, :price, :price_cents
     rename_column :phone_calls, :price, :price_cents
 
+    reversible do |dir|
+      dir.up do
+        PhoneCall.where.not(price_cents: nil).update_all("price_cents = price_cents * 100")
+      end
+    end
+
     add_index(:messages, :price_cents, algorithm: :concurrently)
     add_index(:messages, :price_unit, algorithm: :concurrently)
   end
