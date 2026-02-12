7c20f894b1854c71ad5fcc1129a90d9f6d6149b8
Samnang Chhun <samnang.chhun@gmail.com>
2026-02-12 16:07:35 +0700

    Hide destroy button on nested form if the child has only one (#2451)

--- app/javascript/controllers/remove_nested_form_controller.js
+++ app/javascript/controllers/remove_nested_form_controller.js
@@ -4,6 +4,13 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["destroyElement"];
 
+  NESTED_FORM_GROUP_SELECTOR = ".nested-form-group";
+  REMOVE_BUTTON_SELECTOR = ".remove-button";
+
+  connect() {
+    this.toggleRemoveButton();
+  }
+
   remove(e) {
     e.preventDefault();
 
@@ -11,7 +18,32 @@ export default class extends Controller {
       .querySelectorAll("input:not([type='hidden']), select, textarea")
       .forEach((el) => (el.disabled = true));
 
-    this.element.style.display = "none";
+    this.element.classList.add("d-none");
     this.destroyElementTarget.value = "true";
+
+    this.toggleRemoveButton();
+  }
+
+  toggleRemoveButton() {
+    const activeNestedForms = this.#activeNestedForms();
+    const hideButton = activeNestedForms.length <= 1;
+
+    activeNestedForms.forEach((nestedForm) => {
+      const removeButton = nestedForm.querySelector(this.REMOVE_BUTTON_SELECTOR);
+      removeButton.classList.toggle("d-none", hideButton);
+    });
+  }
+
+  #activeNestedForms() {
+    const nestedFormGroup =
+      this.element.closest(this.NESTED_FORM_GROUP_SELECTOR);
+
+    const nestedForms = Array.from(
+      nestedFormGroup.querySelectorAll(
+        `[data-controller~='${this.identifier}']:not([class*='d-none'])`,
+      ),
+    );
+
+    return nestedForms;
   }
 }
