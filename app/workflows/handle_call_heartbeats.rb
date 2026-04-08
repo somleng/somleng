88186b770792accb653ce3f52509898401fb3d4b
Samnang Chhun <samnang.chhun@gmail.com>
2026-04-08 15:23:03 +0700

    WIP

--- app/workflows/handle_call_heartbeats.rb
+++ app/workflows/handle_call_heartbeats.rb
@@ -1,14 +1,14 @@
 class HandleCallHeartbeats < ApplicationWorkflow
-  attr_reader :call_ids
+  attr_reader :switch_proxy_identifiers
 
-  def initialize(call_ids)
+  def initialize(switch_proxy_identifiers)
     super()
-    @call_ids = call_ids
+    @switch_proxy_identifiers = switch_proxy_identifiers
   end
 
   def call
     PhoneCall
-      .where(switch_proxy_identifier: call_ids)
+      .where(switch_proxy_identifier: switch_proxy_identifiers)
       .update_all(last_heartbeat_at: Time.current)
   end
 end
