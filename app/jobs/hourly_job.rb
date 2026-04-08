9c4897560428799c445783ecdda23768bf199952
Samnang Chhun <samnang.chhun@gmail.com>
2026-04-08 15:17:14 +0700

    WIP

--- app/jobs/hourly_job.rb
+++ app/jobs/hourly_job.rb
@@ -2,7 +2,6 @@ class HourlyJob < ApplicationJob
   queue_as AppSettings.fetch(:aws_sqs_low_priority_queue_name)
 
   def perform
-    ExecuteWorkflowJob.perform_later(ExpireInProgressPhoneCalls.to_s) # TODO: Move to shorter interval job
     ExecuteWorkflowJob.perform_later(ExpireInitiatingPhoneCalls.to_s)
     ExecuteWorkflowJob.perform_later(RevokeSIPTrunkPermissions.to_s)
     ExecuteWorkflowJob.perform_later(RescheduleOutboundCalls.to_s)
