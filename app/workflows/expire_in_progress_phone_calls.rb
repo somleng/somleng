af6a06cd1865537d7b4ec4eed2da1f7e9fb00b95
David Wilkie <dwilkie@gmail.com>
2026-04-20 17:56:34 +0700

    Fix filtering expired phone calls (#2567)

--- app/workflows/expire_in_progress_phone_calls.rb
+++ app/workflows/expire_in_progress_phone_calls.rb
@@ -7,22 +7,23 @@ class ExpireInProgressPhoneCalls < ApplicationWorkflow
   end
 
   def call
+    stale_heartbeat = PhoneCall.in_progress.where(last_heartbeat_at: ..15.minute.ago)
+    no_heartbeat = PhoneCall.in_progress.where(initiated_at: ..1.hours.ago, last_heartbeat_at: nil)
+
+    expire_phone_calls(stale_heartbeat)
+    expire_phone_calls(no_heartbeat)
+  end
+
+  private
+
+  def expire_phone_calls(scope)
     phone_call_ids = []
 
-    expired_phone_calls.find_each do |phone_call|
+    scope.find_each do |phone_call|
       phone_call_ids << phone_call.id
       session_limiters.each { _1.remove_session_from(phone_call.region.alias, scope: phone_call.account_id) }
     end
 
     PhoneCall.where(id: phone_call_ids).update_all(status: :session_timeout)
-  end
-
-  private
-
-  def expired_phone_calls
-    stale_heartbeat = PhoneCall.where(last_heartbeat_at: ..15.minute.ago)
-    no_heartbeat = PhoneCall.where(initiated_at: ..1.hours.ago, last_heartbeat_at: nil)
-
-    PhoneCall.in_progress.merge(stale_heartbeat.or(no_heartbeat))
   end
 end
