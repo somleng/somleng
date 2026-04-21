4b109a08028170dba80d5d783d27e502ecf8806b
Samnang Chhun <samnang.chhun@gmail.com>
2026-04-21 16:40:12 +0700

    Fix slow queries for expiring phone calls (#2573)

--- db/migrate/20260421091752_fix_adding_partial_indexes_on_phone_calls_for_expiring_queries.rb
+++ db/migrate/20260421091752_fix_adding_partial_indexes_on_phone_calls_for_expiring_queries.rb
@@ -1,19 +1,25 @@
-class AddPartialIndexesForPhoneCallExpiryQueries < ActiveRecord::Migration[8.1]
+class FixAddingPartialIndexesOnPhoneCallsForExpiringQueries < ActiveRecord::Migration[8.1]
+  disable_ddl_transaction!
+
   def change
-    # Optimization for calls with stale heartbeats
-    add_index(
+    remove_index(
       :phone_calls,
       [ :sequence_number, :last_heartbeat_at ],
       order: { sequence_number: :desc },
       where: "status IN ('initiated', 'ringing', 'answered')"
     )
 
-    # Optimization for calls that never had a heartbeat
-    add_index(
+    remove_index(
       :phone_calls,
       [ :sequence_number, :initiated_at ],
       order: { sequence_number: :desc },
       where: "status IN ('initiated', 'ringing', 'answered') AND last_heartbeat_at IS NULL"
     )
+
+    add_index :phone_calls,
+      [ :status, :initiated_at, :sequence_number ],
+      order: { sequence_number: :desc },
+      where: "last_heartbeat_at IS NULL",
+      algorithm: :concurrently
   end
 end
