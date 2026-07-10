aa631a626e480e8495f75d00f5a95a59cbbb683e
Samnang Chhun <samnang.chhun@gmail.com>
2026-07-10 16:07:49 +0700

    Add to support Azure TTS

---
+++ config/initializers/tts_voices.rb
@@ -0,0 +1,11 @@
+TTSVoices.configure do |config|
+  if Rails.env.development? || Rails.env.test?
+    # Use stub_responses for Polly
+    config.voices = ["Basic", "Polly"]
+  else
+    config.azure_options = {
+      region: AppSettings.fetch(:azure_tts_region),
+      key: AppSettings.fetch(:azure_tts_key),
+    }
+  end
+end
